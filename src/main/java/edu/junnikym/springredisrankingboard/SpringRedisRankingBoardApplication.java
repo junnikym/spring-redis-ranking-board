b5512863df567b2585a22803795e90e330932f08
junnikym <junnikym@gmail.com>
2022-05-23 23:57:56 +0900

    [+] (readme) Java의 Redis Client

---
+++ src/main/java/edu/junnikym/springredisrankingboard/SpringRedisRankingBoardApplication.java
@@ -0,0 +1,13 @@
+package edu.junnikym.springredisrankingboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisRankingBoardApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(SpringRedisRankingBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/junnikym/springredisrankingboard/SpringRedisRankingBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.junnikym.springredisrankingboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisRankingBoardApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
